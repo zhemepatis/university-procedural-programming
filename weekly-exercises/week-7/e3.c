2eaa66012d9885df9e4261fd64012d6c13b1a4a2
zhemepatis <rink.gab@gmail.com>
2022-10-20 08:33:37 +0300

    Finished 3 exercise

--- weekly-exercises/week-7/e3.c
+++ weekly-exercises/week-7/e3.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
+#include <stdio.h>
+
+int getPositiveNumber(char *msg);
 
 int main() {
 
-
+    int num = getPositiveNumber("Enter a positive integer: ");
+    printf("Result: %d", num);
     
     return 0;
+}
+
+int getPositiveNumber(char *msg) {
+    int number = 0;
+
+    int validInput = 0;
+    while(!validInput) {
+        printf("%s", msg);
+        if(scanf("%d", &number) != 1 || getchar() != '\n') {
+            char temp;
+            scanf("%*[^\n]%c", &temp);
+
+            printf("Error: incorrect input, please try again.\n");
+            continue;
+        }
+        else if(number < 1) {
+            printf("Error: integer is not positive.\n");
+            continue;
+        }
+
+        validInput = 1;
+    }
+
+    return number;
+
 }
