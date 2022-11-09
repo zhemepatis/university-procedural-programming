245789c016d137cc50bdd2e6a2f7c1634e6f73e3
zhemepatis <rink.gab@gmail.com>
2022-11-09 09:12:44 +0200

    Done exercise 5

--- weekly-exercises/week-10/e5.c
+++ weekly-exercises/week-10/e5.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+void includeChars(char string[], char value, int num);
 
 int main() {
+    char string[] = "abc";
+
+    includeChars(string, 'm', 2);
+    assert(strcmp(string, "mmc") == 0);
+
+    includeChars(string, 'g', 0);
+    assert(strcmp(string, "mmc") == 0);
 
     return 0;
+}
+
+void includeChars(char string[], char value, int num) {
+    for(int i = 0; i < num; ++i) {
+        string[i] = value;
+    }
 }
 
